dc71b010359303a38c4519aa5eb618f4188f3156
Leandro <leroccasalvo@hotmail.com>
2022-05-28 00:30:06 -0300

    Add NavBar and ItemListCointainer in App

---
+++ src/components/ItemListContainer.js
@@ -0,0 +1,9 @@
+const ItemListContainer = (props) => {
+    return (
+<h1 style= {{color:'gold'}}>{props.greeting}</h1>
+
+
+)
+}
+
+export default ItemListContainer
---
+++ src/components/NavBar.js
@@ -0,0 +1,16 @@
+import './navBar.css'
+const NavBar = () => {
+    return (
+        <nav>
+            <button style={{color: 'blue'}} >Inicio</button>
+            <button style={{color: 'blue'}}>Camisas</button>
+            <button style={{color: 'blue'}}>Camperas</button>
+            <button style={{color: 'blue'}}>Pantalones</button>
+            <button style={{color: 'blue'}}>Zapatos</button>
+            <button style={{color: 'blue'}}>Accesorios</button>
+            <button style={{color: 'blue'}}>Nosotros</button>
+        </nav>
+    )
+}
+
+export default NavBar
